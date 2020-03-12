31adf61be5fbbdf38dfa6206d859990f8f9e5d4d
Arun Pereira <arunpereira@gmail.com>
2020-03-11 22:33:50 -0400

    Update to 3.13 for private build.

--- martenbuild.cs
+++ martenbuild.cs
@@ -8,7 +8,7 @@ namespace martenbuild
 {
     internal class MartenBuild
     {
-        private const string BUILD_VERSION = "3.12.0";
+        private const string BUILD_VERSION = "3.13.0";
 
         private static void Main(string[] args)
         {
